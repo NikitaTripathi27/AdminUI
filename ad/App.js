f7ebe450a38d552b760d2fa253d3b589ec684d05
Nikita <124888350+NikitaTripathi27@users.noreply.github.com>
2023-05-11 09:22:20 +0530

    Add files via upload

---
+++ ad/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+// import BasicTable from "./Home"
+import BasicTable from "./adminUI"
+function App() {
+  return (
+    <div className="App">
+      <BasicTable />
+    </div>
+  );
+}
+
+export default App;
